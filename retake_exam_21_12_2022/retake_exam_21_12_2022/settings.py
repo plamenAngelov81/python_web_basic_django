e18f71d03cb7b9da8103791e92f4843f06459570
Пламен Ангелов <103038521+plamenAngelov81@users.noreply.github.com>
2022-12-30 15:41:04 +0200

    Update settings.py

--- retake_exam_21_12_2022/retake_exam_21_12_2022/settings.py
+++ retake_exam_21_12_2022/retake_exam_21_12_2022/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-tvc6c-u%y6dkn6*p^j7zv6tigzxm!ird(%aly%u75^kfm2pyba'
+SECRET_KEY = 'purple_pony'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
