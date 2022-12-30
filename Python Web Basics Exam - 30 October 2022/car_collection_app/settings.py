a80af0849d37c18c2229462d339f043de59e075f
Пламен Ангелов <103038521+plamenAngelov81@users.noreply.github.com>
2022-12-30 15:45:15 +0200

    Update settings.py

--- Python Web Basics Exam - 30 October 2022/car_collection_app/settings.py
+++ Python Web Basics Exam - 30 October 2022/car_collection_app/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-z8=e+cm=m^)lgwa34m7+hax96w)^1mfx6ir)4@#m!w5%cx_d8_'
+SECRET_KEY = 'pink_pony'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
